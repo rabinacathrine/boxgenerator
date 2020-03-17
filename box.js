b5c38224aed9967c7bec98d4cdc5cf79ecfd5afa
Rabina <32222576+rabinacathrine@users.noreply.github.com>
2020-03-17 17:57:06 +0530

    prograd

---
+++ box.js
@@ -0,0 +1,17 @@
+let button=document.getElementById('theboxes');
+button.addEventListener("click",myfunction);
+var oldvalue=0;
+var i=1;
+function myfunction(){
+let n=document.getElementById('number').value;
+n=parseInt(oldvalue)+parseInt(n);
+for(;i<=n;i++){
+    var box=document.createElement('div');
+    box.classList.add('mydiv');
+    document.getElementById('box').appendChild(box);
+    box.innerHTML=i;
+
+ oldvalue=document.getElementById("box").lastElementChild.innerHTML;
+    console.log(oldvalue);
+}
+}
